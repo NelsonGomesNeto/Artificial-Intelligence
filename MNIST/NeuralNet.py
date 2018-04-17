bad0a2377eed8ffdf02e4be8d2930ddf92dddc20
Nelson Gomes Neto <ngn@ic.ufal.br>
2018-04-17 06:54:27 -0300

    Almost as better as a coin hahaha

--- MNIST/NeuralNet.py
+++ MNIST/NeuralNet.py
@@ -1,5 +1,6 @@
 import FileReader
 import numpy as np
+import time
 from random import random
 from math import exp
 np.seterr(all="ignore")
@@ -64,29 +65,31 @@ def backPropagation(activation, output, delta, weight, biases):
         #print("after:", weight[i])
     return(reversed(nabla))
 
-def train(image, label, weight, biases, progress):
+def train(image, label, weight, biases, progress, batchSize):
     i, perfect = 0, 0
     while (i < len(image)):
         j, nabla = 0, 0
-        while (j < 10 and j + i < len(image)):
+        while (j < batchSize and j + i < len(image)):
             output, activation = feedForward(image[i + j], weight, biases)
             if (label[i + j] == getAnswer(output)[1]): perfect += 1
-            if (progress): print(i + j, perfect, "label: %d, prediction: %d" % (label[i + j], getAnswer(output)[1]))
             error, delta = getError(label[i + j], output), getDerivative(output, label[i + j])
             newNabla = backPropagation(activation, output, delta, weight, biases)
             if (j == 0): nabla = newNabla
             else: nabla = [n + nn for n, nn in zip(nabla, newNabla)]
             j += 1
+        if (progress and i + j < len(image)): print(i + j, perfect, "label: %d, prediction: %d" % (label[i + j], getAnswer(output)[1]))
         i += j
-        #print("before:", weight)
-        #print("nabla:", *nabla)
-        weight = [w - 100*nw/10 for w, nw in zip(weight, nabla)]
-        #print("after:", weight)
+        print("before:", weight)
+        print("nabla:", *nabla)
+        weight = [w + nw for w, nw in zip(weight, nabla)]
+        print("after:", weight)
+        #time.sleep(10000)
+
 
 netSize = [784, 16, 16, 10]
 weight, biases = [], []
 for i in range(len(netSize) - 1):
-    weight += [[np.array([100*(0.5-random())] * netSize[i]) for j in range(netSize[i + 1])]]
+    weight += [[np.array([2*(0.5-random())] * netSize[i]) for j in range(netSize[i + 1])]]
     biases += [[0 for j in range(netSize[i + 1])]] #0.5-random()
 
 if (DEBUG):
@@ -99,8 +102,6 @@ if (DEBUG):
 image, label = getData()
 print("Loaded data")
 print("Training")
-train(image, label, weight, biases, 1)
-print("Testing")
-train(image, label, weight, biases, 1)
-print(weight[len(netSize) - 2], len(weight[len(netSize) - 2]))
+for i in range(100):
+    train(image, label, weight, biases, 1, 100)
 
