f554f80ecf464cfccfeace2bb4dacaa80f16bd0f
NelsonGomesNeto <ngn@ic.ufal.br>
2018-04-18 15:03:04 -0300

    Visualization Working Nicely

--- MNIST/VisualizationManager.py
+++ MNIST/VisualizationManager.py
@@ -1,15 +1,28 @@
-from PIL import Image
+from PIL import Image, ImageColor
 import os
 import FileManager
 fm = FileManager.FileManager()
 
-def printImage(image, label):
-    for i in range(784):
-        if (i and i % 28 == 0): print()
-        print((image[i] > 0) + 0, end='')
-    print("", label)
+class VisualizationManager:
 
-image = fm.getImages("train-images-idx3-ubyte")
-label = fm.getLabels("train-labels-idx1-ubyte")
+    image, label = 0, 0
 
-printImage(image[0], label[0])
+    def getData(self):
+        image = fm.getImages("t10k-images-idx3-ubyte/data")
+        label = fm.getLabels("t10k-labels-idx1-ubyte/data")
+        return(image, label)
+
+    def consolePrint(self, image, label):
+        for i in range(784):
+            if (i and i % 28 == 0): print()
+            print((image[i] > 0) + 0, end='')
+        print("", label)
+
+    def generateImage(self, image, label):
+        im = Image.new("L", [784, 784])
+        for i in range(784):
+            for j in range(784):
+                im.putpixel([j, i], int(image[((i // 28) * 28) + j // 28] * 255))
+        im.show()
+        return(im)
+
