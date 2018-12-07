e18fa6734d5f2afad87706423a6ea334e0d5bba2
NelsonGomesNeto <ngn@ic.ufal.br>
2018-12-07 15:30:11 -0300

    Needs to improve MiniMax

--- AI2/connect.cpp
+++ AI2/connect.cpp
@@ -20,7 +20,7 @@ int countNeighbours(int i, int j, int di, int dj)
   }
   return(neighbours);
 }
-double evaluateBoard()
+double evaluateBoard(char wanted)
 {
   double value = 0;
   memcpy(aux, board, sizeof(board));
@@ -28,9 +28,9 @@ double evaluateBoard()
     for (int j = 0; j < m; j ++)
       if (board[i][j] != ' ')
       {
-        value += (board[i][j] == 'O' ? 1.25 : -1.0) * pow(countNeighbours(i, j, 0, 1), 2);
-        value += (board[i][j] == 'O' ? 1.25 : -1.0) * pow(countNeighbours(i, j, 1, 0), 2);
-        value += (board[i][j] == 'O' ? 1.25 : -1.0) * pow(countNeighbours(i, j, 1, 1), 2);
+        value += (board[i][j] == wanted ? 1.25 : 0.5) * pow(countNeighbours(i, j, 0, 1), 4); // 4 is the best
+        value += (board[i][j] == wanted ? 1.25 : 0.5) * pow(countNeighbours(i, j, 1, 0), 4);
+        value += (board[i][j] == wanted ? 1.25 : 0.5) * pow(countNeighbours(i, j, 1, 1), 4);
       }
   return(value);
 }
@@ -49,16 +49,16 @@ int winner()
 // If (depth & 1): minimum; else maximum
 // Therefore we'll need to evaluate a leaf somehow
 // Let's first try with an even value, so that maximum will be the last
-Movement miniMax(int depth)
+Movement miniMax(int depth, char player)
 {
-  if (!depth) return(Movement({-1, -1, evaluateBoard()}));
+  if (!depth) return(Movement({-1, -1, evaluateBoard(player)}));
   Movement bestMovement = {-1, -1, ((depth & 1) ? inf : -inf)};
   for (int i = 0; i < n; i ++)
     for (int j = 0; j < m; j ++)
       if (board[i][j] == ' ') // player can put a piece
       {
         board[i][j] = (depth & 1) ? 'O' : 'X';
-        Movement m = miniMax(depth - 1);
+        Movement m = miniMax(depth - 1, player == 'O' ? 'X' : 'O');
         if (((depth & 1) && m.value < bestMovement.value)
         || (!(depth & 1) && m.value > bestMovement.value))
         {
@@ -67,26 +67,26 @@ Movement miniMax(int depth)
         }
         board[i][j] = ' ';
       }
-  if (bestMovement.value == ((depth & 1) ? inf : -inf)) bestMovement.value = evaluateBoard();
+  if (bestMovement.value == ((depth & 1) ? inf : -inf)) bestMovement.value = evaluateBoard(player);
   return(bestMovement);
 }
 
 void play()
 {
-  int available = n * m; int player = 0;
+  int available = n * m; char player = 'O';
   printBoard();
   while (available -- && !winner())
   {
-    Movement mov = miniMax(player == 0 ? 4 : 3);
-    board[mov.i][mov.j] = player == 0 ? 'O' : 'X';
-    printf("Player %d at %d %d\n", player + 1, mov.i, mov.j);
+    Movement mov = miniMax(player == 'O' ? 9 : 8, player);
+    if (mov.i == -1 && mov.j == -1) printf("Something very wrong is happening\n");
+    board[mov.i][mov.j] = player;
+    printf("Player %c at %d %d\n", player, mov.i, mov.j);
     printBoard();
-    printf("Here %d | %.3g\n", winner(), miniMax(4).value);
-    player = 1 - player;
+    player = player == 'O' ? 'X' : 'O';
   }
   int won = winner();
   if (!won) printf("Tie!\n");
-  else printf("Player %d won\n", won);
+  else printf("Player %c won\n", won == 1 ? 'O' : 'X');
 }
 
 int main()
