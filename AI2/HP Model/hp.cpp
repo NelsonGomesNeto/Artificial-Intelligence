ed720384f739fb084b8e65b4c52fc8516eafb881
NelsonGomesNeto <ngn@ic.ufal.br>
2019-01-22 00:04:17 -0300

    Prettyfies code

--- AI2/HP Model/hp.cpp
+++ AI2/HP Model/hp.cpp
@@ -17,6 +17,8 @@ struct State
   set<pair<int, int>> hVisited, pVisited;
   double value = 0, energy = 0;
   bool operator<(const State &b) const { return(value > b.value); }
+  void insert(pair<int, int> position, char protein) { if (protein == 'P') pVisited.insert(position); else hVisited.insert(position); i = position.first, j = position.second; }
+  bool has(pair<int, int> position) { return(hVisited.count(position) || pVisited.count(position)); }
 };
 void fillTable(State &state)
 {
@@ -64,8 +66,8 @@ void printState(State &state)
 
 State bfs()
 {
-  State bestState; bestState.value = 1e7;
-  State base; if (hp[0] == 'P') base.pVisited.insert({0, 0}); else base.hVisited.insert({0, 0});
+  State bestState; bestState.energy = 1e7;
+  State base; base.insert({0, 0}, hp[0]);
   queue<State> q; q.push(base);
   while (!q.empty())
   {
@@ -73,28 +75,19 @@ State bfs()
     // printState(u);
     if (u.direction.size() == hpSize - 1)
     {
-      double energy = stateEnergy(u, true); u.value = energy;
-      if (u.value < bestState.value) bestState = u;
+      if (u.energy < bestState.energy) bestState = u;
       continue;
     }
+
     for (int k = 0; k < 4; k ++)
     {
       char nextProtein = hp[u.direction.size() + 1];
       int ii = u.i + dy[k], jj = u.j + dx[k];
-      if ((nextProtein == 'P' && !u.pVisited.count({ii, jj})) || (nextProtein == 'H' && !u.hVisited.count({ii, jj})))
+      if (!u.has({ii, jj}))
       {
         State next = u; next.direction.push_back(k);
-        next.i = ii, next.j = jj;
-        if (nextProtein == 'P') next.pVisited.insert({ii, jj});
-        else
-        {
-          next.hVisited.insert({ii, jj});
-          next.value = u.value + (hp[u.direction.size()] == 'H');
-          for (int kk = 0; kk < 4; kk ++) next.value -= next.hVisited.count({ii + dy[kk], jj + dx[kk]});
-        }
-        // next.visited.insert({next.i, next.j}), next.value = -u.value*0.05 + ((accumulatedEnergy[next.direction.size() + 1] - 1)*1 + (stateEnergy(next, true) - baseEnergy)) / (accumulatedEnergy[next.direction.size() + 1]);
-        printf("%lf\n", next.value);
-        //-u.value*0 + stateEnergy(next, true) + accumulatedEnergy[next.direction.size() + 1] * 1;// accumulatedEnergy[next.direction.size()];
+        next.insert({ii, jj}, nextProtein);
+        if (nextProtein == 'H') if (nextProtein == 'H') for (int kk = 0, prevK = (k + 2) % 4; kk < 4; kk ++) if (prevK != kk) next.energy -= next.hVisited.count({ii + dy[kk], jj + dx[kk]});
         q.push(next);
       }
     }
@@ -106,38 +99,30 @@ State Astar()
 {
   int breakCount = 1000;
   State bestState; bestState.energy = 1e7;
-  State base; if (hp[0] == 'P') base.pVisited.insert({0, 0}); else base.hVisited.insert({0, 0});
+  State base; base.insert({0, 0}, hp[0]);
   priority_queue<State> q; q.push(base);
   while (!q.empty())
   {
     State u = q.top(); q.pop();
     // printState(u);
-    if (u.direction.size() == hpSize - 1)
+    if (u.direction.size() == hpSize - 1) // Reached the end
     {
-      if (u.energy <= bestState.energy) bestState = u;
-      else breakCount --;
+      if (u.energy < bestState.energy) bestState = u;
+      else if (u.energy > bestState.energy) breakCount --;
       if (breakCount == 0) break;
       continue;
     }
-    for (int k = 0; k < 4; k ++)
+
+    for (int k = 0; k < 4; k ++) // Adds each possible direction
     {
       char nextProtein = hp[u.direction.size() + 1];
       int ii = u.i + dy[k], jj = u.j + dx[k];
-      if (!u.pVisited.count({ii, jj}) && !u.hVisited.count({ii, jj}))
+      if (!u.has({ii, jj}))
       {
         State next = u; next.direction.push_back(k);
-        next.i = ii, next.j = jj;
-        if (nextProtein == 'P') next.pVisited.insert({ii, jj});
-        else
-        {
-          next.hVisited.insert({ii, jj});
-          next.energy = u.energy; // + (hp[u.direction.size()] == 'H');
-          for (int kk = 0, prevK = (k + 2) % 4; kk < 4; kk ++) if (prevK != kk) next.energy -= next.hVisited.count({ii + dy[kk], jj + dx[kk]});
-          next.value = u.value*0 + next.energy / (4*accumulatedEnergy[next.direction.size()]);
-        }
-        // next.visited.insert({next.i, next.j}), next.value = -u.value*0.05 + ((accumulatedEnergy[next.direction.size() + 1] - 1)*1 + (stateEnergy(next, true) - baseEnergy)) / (accumulatedEnergy[next.direction.size() + 1]);
-        // printf("%lf\n", next.value);
-        //-u.value*0 + stateEnergy(next, true) + accumulatedEnergy[next.direction.size() + 1] * 1;// accumulatedEnergy[next.direction.size()];
+        next.insert({ii, jj}, nextProtein);
+        if (nextProtein == 'H') for (int kk = 0, prevK = (k + 2) % 4; kk < 4; kk ++) if (prevK != kk) next.energy -= next.hVisited.count({ii + dy[kk], jj + dx[kk]});
+        next.value = u.value*0 + next.energy / (4*accumulatedEnergy[next.direction.size()]); // Heuristic value
         q.push(next);
       }
     }
@@ -164,8 +149,8 @@ int main()
     // State best = bfs();
     State best = Astar();
     printf("Best energy: %.0lf\n", stateEnergy(best, true));
+    printState(best);
     fflush(stdout);
-    // printState(best);
   }
 
   return(0);
